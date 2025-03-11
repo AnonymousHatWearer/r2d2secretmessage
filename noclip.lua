2180e43b046aa28839b3c5192c606cd170e0523f
AnonymousHatWearer <isaac@e-intel.co.uk>
2025-03-11 05:38:53 +0000

    Update noclip.lua

--- noclip.lua
+++ noclip.lua
@@ -2,7 +2,10 @@ local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 
 for _, part in pairs(character:GetDescendants()) do
-    if part:IsA("BasePart") then
-        part.CanCollide = false 
+    while true do
+        task.wait(0.2)
+        if part:IsA("BasePart") then
+            part.CanCollide = false 
+        
     end
 end
