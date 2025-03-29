8d5fd4c1960121c5c9108de886ac6e39f700f5c8
AnonymousHatWearer <isaac@e-intel.co.uk>
2025-03-29 19:47:54 +0000

    Update noclip.lua

--- noclip.lua
+++ noclip.lua
@@ -3,8 +3,9 @@ local character = player.Character or player.CharacterAdded:Wait()
 
 while true do
     for _, part in pairs(character:GetDescendants()) do
-    task.wait(0.2)
-    if part:IsA("BasePart") then
-        part.CanCollide = false 
+        if part:IsA("BasePart") then
+            part.CanCollide = false
+        end
     end
+    task.wait(0.2) -- Wait outside the loop to avoid unnecessary delays per iteration
 end
