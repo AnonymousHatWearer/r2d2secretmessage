ef9e3f08fb825bbc59e281de93e67303ee086702
AnonymousHatWearer <isaac@e-intel.co.uk>
2025-03-29 20:34:10 +0000

    Update awscolor.lua

--- awscolor.lua
+++ awscolor.lua
@@ -7,18 +7,34 @@ while true do
 		"Medium stone grey",
 		"Light stone grey"
 	}
+	local faces = {
+		"http://www.roblox.com/asset/?id=7075492",
+		"http://www.roblox.com/asset/?id=66329844",
+		"http://www.roblox.com/asset/?id=12777582",
+		"http://www.roblox.com/asset/?id=10747392",
+		"http://www.roblox.com/asset/?id=83017053"
+		
+	}
+	
 	local randomIndex = math.random(1, #colors)
 	game.Workspace[player.Name].Head.BrickColor = BrickColor.new(colors[randomIndex])
-	local randomIndex = math.random(1, #colors)
+	randomIndex = math.random(1, #colors)
 	game.Workspace[player.Name]["Left Leg"].BrickColor = BrickColor.new(colors[randomIndex])
-	local randomIndex = math.random(1, #colors)
+	randomIndex = math.random(1, #colors)
 	game.Workspace[player.Name]["Right Leg"].BrickColor = BrickColor.new(colors[randomIndex])
-	local randomIndex = math.random(1, #colors)
+	randomIndex = math.random(1, #colors)
 	game.Workspace[player.Name]["Left Arm"].BrickColor = BrickColor.new(colors[randomIndex])
-	local randomIndex = math.random(1, #colors)
+	randomIndex = math.random(1, #colors)
 	game.Workspace[player.Name]["Right Arm"].BrickColor = BrickColor.new(colors[randomIndex])
-	local randomIndex = math.random(1, #colors)
+	randomIndex = math.random(1, #colors)
 	game.Workspace[player.Name].Torso.BrickColor = BrickColor.new(colors[randomIndex])
+	
+	local randomFaceIndex = math.random(1, #faces)
+	local head = character.Head
+	local face = head:FindFirstChild("face")
 
+	if face then
+		face.Texture = faces[randomFaceIndex]
+	end
 	wait(0.1)
 end
