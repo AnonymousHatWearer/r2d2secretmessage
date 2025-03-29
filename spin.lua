13ffe5953defb85f7c8c5fa200239b47a00ca5a0
AnonymousHatWearer <isaac@e-intel.co.uk>
2025-03-29 19:41:25 +0000

    Update spin.lua

--- spin.lua
+++ spin.lua
@@ -1,10 +1,15 @@
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
+local e = 0
 local function spinCharacter()
     while true do
-        character:SetPrimaryPartCFrame(character.PrimaryPart.CFrame * CFrame.Angles(0, math.rad(9000), 0))
+        character:SetPrimaryPartCFrame(character.PrimaryPart.CFrame * CFrame.Angles(0, math.rad(1000), 0))
         wait(0.02)
     end
 end
-spinCharacter()
+repeat
+    spinCharacter()
+    e = e + 1
+    wait(0.4)
+until e == 9
